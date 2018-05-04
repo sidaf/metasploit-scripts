6e4738ddc4bcb4f0d68563e8ca7b16cbf97ec031
sidaf <sion.dafydd@gmail.com>
2018-05-04 13:20:15 +0100

    Fixes

--- modules/auxiliary/scanner/http/files_dir2.rb
+++ modules/auxiliary/scanner/http/files_dir2.rb
@@ -27,7 +27,7 @@ class MetasploitModule < Msf::Auxiliary
           Opt::RPORT(80),
           OptString.new('VHOST', [ false, "HTTP server virtual host" ]),
           OptBool.new('SSL', [ false, 'Negotiate SSL/TLS for outgoing connections', false]),
-          OptString.new('PATH', [ true,  "The path  to identify files", '/']),
+          OptString.new('PATH', [ true,  "Base path to identify files", '/']),
           OptBool.new('APPENDEXT', [ false, "Append file extensions", true]),
           OptString.new('EXT', [ false, "Append additional file extension", '']),
           OptPath.new('DICTIONARY', [ false, "Path of word dictionary to use", File.join(Msf::Config.data_directory, "wmap", "wmap_files.txt") ])
@@ -39,7 +39,7 @@ class MetasploitModule < Msf::Auxiliary
           OptString.new('UserAgent', [false, 'The User-Agent header to use for all requests', Rex::Proto::Http::Client::DefaultUserAgent ]),
           OptInt.new('ErrorCode', [ true,  "The expected HTTP status code for non existant files", 404]),
           OptPath.new('HTTP404Sigs', [ false, "Path of 404 signatures to use", File.join(Msf::Config.data_directory, "wmap", "wmap_404s.txt") ]),
-          OptInt.new('MaxThreads', [ true, "The maximum number of concurrent requests", 25])
+          OptInt.new('MaxThreads', [ true, "The maximum number of concurrent requests", 10])
       ]
     )
 
@@ -94,81 +94,87 @@ class MetasploitModule < Msf::Auxiliary
     extensions << datastore['EXT'] unless datastore['EXT'].empty?
 
     if not datastore['APPENDEXT']
-      extensions = [ '' ]
+      extensions = ['']
     end
 
+
+    queue = Queue.new
     extensions.each do |ext|
-      queue = Queue.new
       File.open(datastore['DICTIONARY'], 'rb').each_line do |testf|
-        file = testf.strip                    # remove newline characters
-        file = dir[1..-1] if file[0,1] == '/' # remove leading slash if it exists
+        file = testf.strip                      # remove newline characters
+        file = file[1..-1] if file[0,1] == '/' # remove leading slash if it exists
         queue.push "#{file}#{ext}"
       end
+    end
 
-      #
-      # Detect error code
-      #
-      if ecode.zero?
-        random_file = Rex::Text.rand_text_alpha(8).chomp
+    #
+    # Detect error code
+    #
+    if ecode.zero?
+      random_file = Rex::Text.rand_text_alpha(8).chomp
 
-        url = "#{(ssl ? 'https' : 'http')}://#{vhost}:#{rport}#{base_path}#{random_file}#{ext}"
-        resolve = Ethon::Curl.slist_append(nil, "#{vhost}:#{rport}:#{rhost}")
-
-        Typhoeus::Config.user_agent = datastore['UserAgent']
-
-        request = Typhoeus::Request.new(
-            url,
-            resolve: resolve,
-            method: 'GET',
-            followlocation: false,
-            connecttimeout: 5,
-            timeout: 10,
-            ssl_verifyhost: 0,
-            ssl_verifypeer: false
-        )
-        response = request.run
-
-        if response.timed_out?
-          print_error("Unable to connect, connection timed out (#{wmap_target_host})")
-          return
-        end
-
-        if response.code.zero?
-          print_error("Unable to connect, could not get a http response (#{wmap_target_host})")
-          return
-        end
-
-        # Look for a string we can signature on as well
-        if response.code >= 200 and response.code <= 299
-          emesg = nil
-          File.open(datastore['HTTP404Sigs'], 'rb').each do |str|
-            if response.body.index(str)
-              emesg = str
-              break
-            end
-          end
-
-          if not emesg
-            print_status("Using first 256 bytes of the response as 404 string for files with extension '#{ext}'")
-            emesg = response.body[0,256]
-          else
-            print_status("Using custom 404 string of '#{emesg}' for files with extension '#{ext}'")
-          end
-        else
-          ecode = response.code.to_i
-          print_status("Using code '#{ecode}' as not found for files with extension '#{ext}'")
-        end
-      end
-
-      #
-      # Start testing
-      #
-      Typhoeus::Config.user_agent = datastore['UserAgent']
-      hydra = Typhoeus::Hydra.new(:max_concurrency => num_threads)
+      baseurl = "#{(ssl ? 'https' : 'http')}://#{vhost}:#{rport}#{base_path}"
+      url = "#{baseurl}#{random_file}"
       resolve = Ethon::Curl.slist_append(nil, "#{vhost}:#{rport}:#{rhost}")
 
+      Typhoeus::Config.user_agent = datastore['UserAgent']
+
+      request = Typhoeus::Request.new(
+          url,
+          resolve: resolve,
+          method: 'GET',
+          followlocation: false,
+          connecttimeout: 5,
+          timeout: 10,
+          ssl_verifyhost: 0,
+          ssl_verifypeer: false
+      )
+      response = request.run
+
+      if response.timed_out?
+        print_error("TMO - #{rhost} - #{baseurl}")
+        # move on to next probe
+        next
+      end
+
+      if response.code.zero?
+        print_error("ERR - #{rhost} - #{baseurl}")
+        # move on to next probe
+        next
+      end
+
+      # Look for a string we can signature on as well
+      if response.code >= 200 and response.code <= 299
+        emesg = nil
+        File.open(datastore['HTTP404Sigs'], 'rb').each do |str|
+          if response.body.index(str)
+            emesg = str
+            break
+          end
+        end
+
+        if not emesg
+          print_status("Using first 256 bytes of the response as 404 string for #{baseurl} (#{rhost})")
+          emesg = response.body[0,256]
+        else
+          print_status("Using custom 404 string of '#{emesg}' for #{baseurl} (#{rhost})")
+        end
+      else
+        ecode = response.code.to_i
+        print_status("Using code '#{ecode}' as not found for #{baseurl} (#{rhost})")
+      end
+    end
+
+    #
+    # Start testing
+    #
+    Typhoeus::Config.user_agent = datastore['UserAgent']
+    hydra = Typhoeus::Hydra.new(:max_concurrency => num_threads)
+    resolve = Ethon::Curl.slist_append(nil, "#{vhost}:#{rport}:#{rhost}")
+
+    begin
       while testfile = queue.pop(true)
-        testurl = "#{(ssl ? 'https' : 'http')}://#{vhost}:#{rport}#{base_path}#{testfile}#{ext}"
+        testurl = "#{(ssl ? 'https' : 'http')}://#{vhost}:#{rport}#{base_path}#{testfile}"
 
         request = Typhoeus::Request.new(
             testurl,
@@ -183,26 +189,29 @@ class MetasploitModule < Msf::Auxiliary
 
         request.on_complete do |response|
           if response.timed_out?
-            print_error("#{wmap_base_url}#{base_path}#{testfile}#{ext}, connection timed out (#{wmap_target_host})")
-            return
+            print_error("TMO - #{rhost} - #{response.request.url}")
+            # move on to next probe
+            next
           end
 
           if response.code.zero?
-            print_error("#{wmap_base_url}#{base_path}#{testfile}#{ext}, could not get a http response (#{wmap_target_host})")
-            return
+            print_error("ERR - #{rhost} - #{response.request.url}")
+            # move on to next probe
+            next
           end
+
+          msg = "#{response.code || "ERR"} - #{rhost} - #{response.request.url}"
 
           # check if 404 or error code
           if (response.code == ecode) || (emesg && response.body.index(emesg))
-            vprint_status("#{wmap_base_url}#{base_path}#{testfile}#{ext} [#{response.code}]")
-            return
+            vprint_status(msg)
           else
             report_web_vuln(
                 :host	       => rhost,
                 :port	       => rport,
                 :vhost       => vhost,
                 :ssl         => ssl,
-                :path	       => "#{base_path}#{testfile}#{ext}",
+                :path	       => "#{base_path}#{testfile}",
                 :method      => 'GET',
                 :pname       => '',
                 :proof       => "Res code: #{response.code.to_s}",
@@ -213,10 +222,10 @@ class MetasploitModule < Msf::Auxiliary
                 :name        => 'file'
             )
 
-            print_good("Found #{wmap_base_url}#{base_path}#{testfile}#{ext} [#{response.code}] (#{wmap_target_host})")
+            print_good(msg)
 
             if response.code.to_i == 401
-              print_status("#{wmap_base_url}#{base_path}#{testfile}#{ext} requires authentication: #{response.headers['WWW-Authenticate']} (#{wmap_target_host})")
+              print_good((" " * 24) + "WWW-Authenticate: #{response.headers['WWW-Authenticate']}")
 
               report_note(
                   :host	  => rhost,
@@ -224,7 +233,7 @@ class MetasploitModule < Msf::Auxiliary
                   :proto  => 'tcp',
                   :sname	=> (ssl ? 'https' : 'http'),
                   :type	  => 'WWW_AUTHENTICATE',
-                  :data	  => "#{wmap_base_url}#{base_path}#{testfile}#{ext} Auth: #{response.headers['WWW-Authenticate']}",
+                  :data	  => "#{wmap_base_url}#{base_path}#{testdir} Auth: #{response.headers['WWW-Authenticate']}",
                   :update => :unique_data
               )
             end
@@ -236,82 +245,85 @@ class MetasploitModule < Msf::Auxiliary
 
         hydra.queue request
       end
-
-      hydra.run
+    rescue ThreadError
+    rescue => e
+      puts e.backtrace
+      raise
     end
 
-    def vhost
-      datastore['VHOST'] || datastore['RHOST']
+    hydra.run
+  end
+
+  def vhost
+    datastore['VHOST'] || datastore['RHOST']
+  end
+
+  def rhost
+    datastore['RHOST']
+  end
+
+  def rport
+    datastore['RPORT']
+  end
+
+  def ssl
+    datastore['SSL']
+  end
+
+  #
+  # Returns a modified version of the URI that:
+  # 1. Always has a starting slash
+  # 2. Removes all the double slashes
+  #
+  def normalize_uri(*strs)
+    new_str = strs * "/"
+
+    new_str = new_str.gsub!("//", "/") while new_str.index("//")
+
+    # Makes sure there's a starting slash
+    unless new_str[0,1] == '/'
+      new_str = '/' + new_str
     end
 
-    def rhost
-      datastore['RHOST']
+    new_str
+  end
+
+  def report(response)
+    # Report a website to the database
+    site = report_web_site(:wait => true, :host => rhost, :port => rport, :vhost => vhost, :ssl => datastore['SSL'])
+
+    uri = URI.parse(response.request.url)
+    info = {
+        :web_site => site,
+        :path     => uri.path,
+        :query    => uri.query,
+        :code     => response.code,
+        :body     => response.body,
+        :headers  => response.headers
+    }
+
+    if response.headers['content-type']
+      info[:ctype] = page.headers['content-type']
     end
 
-    def rport
-      datastore['RPORT']
+    # TODO
+    #if !page.cookies.empty?
+    #  info[:cookie] = page.cookies
+    #end
+
+    if response.headers['authorization']
+      info[:auth] = response.headers['authorization']
     end
 
-    def ssl
-      datastore['SSL']
+    if response.headers['location']
+      info[:location] = response.headers['location']
     end
 
-    #
-    # Returns a modified version of the URI that:
-    # 1. Always has a starting slash
-    # 2. Removes all the double slashes
-    #
-    def normalize_uri(*strs)
-      new_str = strs * "/"
-
-      new_str = new_str.gsub!("//", "/") while new_str.index("//")
-
-      # Makes sure there's a starting slash
-      unless new_str[0,1] == '/'
-        new_str = '/' + new_str
-      end
-
-      new_str
+    if response.headers['last-modified']
+      info[:mtime] = response.headers['last-modified']
     end
 
-    def report(response)
-      # Report a website to the database
-      site = report_web_site(:wait => true, :host => rhost, :port => rport, :vhost => vhost, :ssl => ssl)
-
-      uri = URI.parse(response.url)
-      info = {
-          :web_site => site,
-          :path     => uri.path,
-          :query    => uri.query,
-          :code     => response.code,
-          :body     => response.body,
-          :headers  => response.headers
-      }
-
-      if response.headers['content-type']
-        info[:ctype] = page.headers['content-type']
-      end
-
-      # TODO
-      #if !page.cookies.empty?
-      #  info[:cookie] = page.cookies
-      #end
-
-      if response.headers['authorization']
-        info[:auth] = response.headers['authorization']
-      end
-
-      if response.headers['location']
-        info[:location] = response.headers['location']
-      end
-
-      if response.headers['last-modified']
-        info[:mtime] = response.headers['last-modified']
-      end
-
-      # Report the web page to the database
-      report_web_page(info)
-
-    end
+    # Report the web page to the database
+    report_web_page(info)
   end
 end
